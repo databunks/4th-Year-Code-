26441fe9d2d2931778e5be425069386e0a0124aa
databunks <sahaq2001@gmail.com>
2022-11-19 22:43:01 +0000

    finished code

--- Machine Learning/Assignment 2/Assignment2.py
+++ Machine Learning/Assignment 2/Assignment2.py
@@ -1,9 +1,10 @@
-import pandas as pd
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import KFold
 import numpy as np
+import pandas as pd
+from KNeighbours import KNeighboursRegressorFunction
+from DecisionTreeRegression import DecisionTreeRegressionFunction
 
-# Details for wildfire dataset
+
+# Details for steel dataset
 datasetFileName = "steel.csv"
 independent_cols = ["normalising_temperature","tempering_temperature","percent_silicon","percent_chromium","percent_copper","percent_nickel","percent_sulphur","percent_carbon","percent_manganese"]
 dependent_col = "tensile_strength"
@@ -11,15 +12,12 @@ dependent_col = "tensile_strength"
 # Load in dataset
 dataset = pd.read_csv(datasetFileName)
 
+# Create 2 arrays which contain the independent and dependent variables
 X = np.array(dataset.loc[:,independent_cols])
 y = np.array(dataset.loc[:,dependent_col])
 
-kf = KFold(n_splits=10)
+KNeighboursRegressorFunction(X,y)
+DecisionTreeRegressionFunction(X,y)
 
-for train_index, test_index in kf.split(X):
-
-    #print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
 
 
---
+++ Machine Learning/Assignment 2/KNeighbours.py
@@ -0,0 +1,107 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+
+
+
+def PlotGraph(j_values, training_R_values, training_MAE_values, test_R_values, test_MAE_values):
+
+    plt.scatter(j_values, training_R_values[0], marker="x")
+    plt.scatter(j_values, test_R_values[0], marker="+")
+    plt.scatter(j_values, training_MAE_values[0], marker="^")
+    plt.scatter(j_values, test_MAE_values[0], marker="v")
+
+    plt.scatter(j_values, training_R_values[1], marker="p")
+    plt.scatter(j_values, test_R_values[1], marker="*")
+    plt.scatter(j_values, training_MAE_values[1], marker="D")
+    plt.scatter(j_values, test_MAE_values[1], marker="d")
+
+    plt.xlim([0, max(j_values)+2])
+    plt.ylim([-100, 100])
+    plt.xlabel("Value of j")
+    plt.ylabel("Accuracy score based on mean, std, MAE")
+    legend_labels = ["Training mean R value", "Test mean R value", "Training mean MAE value", "Test mean MAE value", "Training std R value", "Test std R value", "Training std MAE value", "Test std MAE value"]
+    plt.legend(labels=legend_labels, loc=4, borderpad=1)
+    plt.title("Effect of j on training and test set accuracy (mean, std, MAE)", fontsize=10)
+    plt.show()
+
+
+
+    
+    
+def KNeighboursRegressorFunction(X,y):
+
+    j_values = list(range(1,50,5))
+    k_values = ["auto", "ball_tree", "kd_tree", "brute"]
+
+    KNNRegressor = KNeighborsRegressor()
+    parameters = {'algorithm': (*k_values,), 'n_neighbors': j_values, 'n_jobs': [-1]}
+
+    # Mae is domain dependent
+    # R is domain independent
+    
+    clf = GridSearchCV(KNNRegressor, parameters, cv=10, refit="R", return_train_score=True, scoring={"MAE":"neg_mean_absolute_error", "R": "r2"})
+
+    # Fit the regressor model
+    clf.fit(X,y)
+
+    training_mean_R_value = np.round((clf.cv_results_['mean_train_R'])[:10], 3)
+    training_std_R_value = np.round((clf.cv_results_['std_train_R'])[:10], 3)
+   
+    test_mean_R_value = np.round((clf.cv_results_['mean_test_R'])[:10], 3)
+    test_std_R_value = np.round((clf.cv_results_['std_test_R'])[:10], 3)
+
+    training_mean_MAE_value = np.round((clf.cv_results_['mean_train_MAE'])[:10],3)
+    training_std_MAE_value = np.round((clf.cv_results_['std_train_MAE'])[:10],3)
+
+    test_mean_MAE_value = np.round((clf.cv_results_['mean_test_MAE'])[:10],3)
+    test_std_MAE_value = np.round((clf.cv_results_['std_test_MAE'])[:10],3)
+
+    
+    print(f"\nBest Parameters for KNN Regressor:\n {clf.best_params_}\n")
+    print(f"Best score for KNN Regressor:\n {clf.best_score_:.3f}\n")
+
+    print(f"Mean r2 for KNN Regressor (training):\n {training_mean_R_value}\n")
+    print(f"Std r2 for KNN Regressor (training):\n {training_std_R_value}\n")
+
+    print(f"Mean r2 for KNN Regressor (test):\n {test_mean_R_value}\n")
+    print(f"Std r2 for KNN Regressor (test):\n {test_std_R_value}\n")
+
+    print(f"Mean MAE for KNN Regressor (training):\n {training_mean_MAE_value}\n")
+    print(f"Std MAE for KNN Regressor (training):\n {training_std_MAE_value}\n")
+
+    print(f"Mean MAE for KNN Regressor (test):\n {test_mean_MAE_value}\n")
+    print(f"Std MAE for KNN Regressor (test):\n {test_std_MAE_value}\n")
+
+
+    training_R_values = [training_mean_R_value, training_std_R_value]
+    training_MAE_values = [training_mean_MAE_value, training_std_MAE_value]
+
+    test_R_values = [test_mean_R_value, test_std_R_value]
+    test_MAE_values = [test_mean_MAE_value, test_std_MAE_value]
+
+    
+    PlotGraph(j_values, training_R_values, training_MAE_values, test_R_values, test_MAE_values)
+
+
+    
+    
+
+
+
+
+
+
+
+        
+        
+
+
+      
+
+
+
+
+
+
